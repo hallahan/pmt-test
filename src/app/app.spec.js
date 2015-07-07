aeffd8164ea6385e7583e1c5d297fbf51f40b486
Shawna Paradee <sparadee@spatialdev.com>
2015-07-07 14:22:03 -0700

    learning karma

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,15 +1,15 @@
-describe( 'AppCtrl', function() {
-    beforeEach(module('pmtViewer'));
-    var AppCtrl, scope;
-
-    beforeEach(inject(function ($rootScope, $controller) {
-        scope = $rootScope.$new();
-        AppCtrl = $controller('AppCtrl', {
-            $scope: scope
-        });
-    }));
-
-    it('should have a greeing', function () {
-        expect(scope.greeting).toBeDefined();
-    });
+describe('AppCtrl', function () {
+    //var AppCtrl, $location, $scope;
+    
+    //beforeEach(module('pmtViewer'));
+    
+    //beforeEach(inject(function ($controller, _$location_, $rootScope) {
+    //    $location = _$location_;
+    //    $scope = $rootScope.$new();
+    //    AppCtrl = $controller('AppCtrl', { $location: $location, $scope: $scope });
+    //}));
+    
+    //it('should pass a dummy test', function () {
+    //    expect(AppCtrl).toBeUndefined();
+    //});
 });
