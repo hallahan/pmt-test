598499988a93df71bf0bf756900f65ffd01944b5
Shawna Paradee <sparadee@spatialdev.com>
2015-07-23 10:31:04 -0700

    updating docs

--- app.config.js
+++ app.config.js
@@ -41,7 +41,7 @@ module.exports = {
                     },
                     about: {
                         info: {
-                            enable: true
+                            enable: false
                         },
                         map: {
                             enable: true,
