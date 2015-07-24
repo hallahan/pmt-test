4294b6526e64bb74f9837215a721eb93b072a3ae
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-07-24 15:17:08 -0700

    remove grunt coffee

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,26 +217,6 @@ module.exports = function (grunt) {
         },
 
         /**
-         * `grunt coffee` compiles the CoffeeScript sources. To work well with the
-         * rest of the build, we have a separate compilation task for sources and
-         * specs so they can go to different places. For example, we need the
-         * sources to live with the rest of the copied JavaScript so we can include
-         * it in the final build, but we don't want to include our specs there.
-         */
-        coffee: {
-            source: {
-                options: {
-                    bare: true
-                },
-                expand: true,
-                cwd: '.',
-                src: ['<%= app_files.coffee %>'],
-                dest: '<%= build_dir %>',
-                ext: '.js'
-            }
-        },
-
-        /**
          * `ngAnnotate` annotates the sources before minifying. That is, it allows us
          * to code without the array syntax.
          */
