aa4eb68dbc8cd68e94856cd4fe50960d2a0c36cd
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-07-27 14:32:31 -0700

    index properly gets replaced in build dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -398,7 +398,7 @@ module.exports = function (grunt) {
                     parsePattern: /\[\[\s?([\.\-\w]*)\s?\]\]/g
                 },
                 files: {
-                    'build/index2.html': 'build/index.html'
+                    'build/index.html': 'build/index.html'
                 }
             }
         },
