271c5ae50d6f4d8b9d91673eb88d5007fda107d2
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-07-28 10:39:09 -0700

    no need to copy template files

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,16 +162,6 @@ module.exports = function (grunt) {
                   }
                 ]
             },
-            build_tpl: {
-                files: [
-                    {
-                        src: ['<%= app_files.tpl %>'],
-                        dest: '<%= build_dir %>/',
-                        cwd: '.',
-                        expand: true
-                    }
-                ]
-            },
             compile_assets: {
                 files: [
                   {
@@ -553,7 +543,7 @@ module.exports = function (grunt) {
     grunt.registerTask('build', [
       'clean', 'ngconstant:'+ theme, 'html2js', 'jshint', 'less:build',
       'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
-      'copy:build_appjs', 'copy:build_vendorjs', 'copy:build_vendorcss', 'copy:build_tpl', 'index:build', 'bake:build', 'karmaconfig',
+      'copy:build_appjs', 'copy:build_vendorjs', 'copy:build_vendorcss', 'index:build', 'bake:build', 'karmaconfig',
       'karma:continuous'
     ]);
 
